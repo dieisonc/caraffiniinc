a98dd756193b99b0c274ffa6de478a192c2eb660
dieisonc <dieisoncaraffini@gmail.com>
2025-11-10 00:06:46 -0300

    asdasdasd

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import './styles/index.css'
-import { useAuth } from './lib/authStore'
-function Init(){ const init = useAuth((s)=>s.init); React.useEffect(()=>{ init() }, []); return <App/> }
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><BrowserRouter><Init/></BrowserRouter></React.StrictMode>)
+import './styles/global.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
